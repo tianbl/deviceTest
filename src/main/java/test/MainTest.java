7db4898aafe5db49b9624cccb7e64571ee386c6f
tianbaolei <123456bl>
2015-11-05 14:24:18 +0800

    Initial commit

---
+++ src/main/java/test/MainTest.java
@@ -0,0 +1,10 @@
+package test;
+
+/**
+ * Created by tianbaolei on 15-11-5.
+ */
+public class MainTest {
+    public static void main(String[] args){
+        System.out.println("阿三；打！");
+    }
+}
