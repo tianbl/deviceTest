413a33d42fcbd8b13ae44f9348897228d24c9ffa
{tianbl} <{1552802825@qq.com}>
2015-11-19 16:22:49 +0800

    1.修改因楼下文档未更新导致的电力线扩展器无线获取bug

--- src/main/java/cn/com/eastsoft/action/powerlineImpl/WirelessRouteAndExpander.java
+++ src/main/java/cn/com/eastsoft/action/powerlineImpl/WirelessRouteAndExpander.java
@@ -40,7 +40,7 @@ public class WirelessRouteAndExpander extends PowerLine {
             MainJFrame.showMssageln("数据失败，无法获数据库中对应本设备的数据！");
             return false;
         } else {
-            if("U".equals(deviceInfo.get("MAClabel"))){
+            if ("U".equals(deviceInfo.get("MAClabel"))) {
                 MainJFrame.showMssageln("本条条码对应资源数据已经使用,结束操作！");
                 return false;
             }
@@ -61,7 +61,7 @@ public class WirelessRouteAndExpander extends PowerLine {
                 generalSet.getDevice_IP(), connectParamSet.getSshPort());
         String[] setSnKey = {"snkey set --sn=", "snkey set --dkey="};
         String[] querySnKey = {"snkey show --sn", "snkey show --dkey"};
-        String[] infoKey = {"sn", "devicekey","mac_1","dak"};
+        String[] infoKey = {"sn", "devicekey", "mac_1", "dak"};
         MainJFrame.showMssageln("清除production分区...");
         sshClient.executtingCmd("mtd erase production");
         String res = sshClient.executeCmd("snkey set --dkey=" + deviceInfo.get(infoKey[1]) + " --sn=" + deviceInfo.get(infoKey[0]));
@@ -70,7 +70,7 @@ public class WirelessRouteAndExpander extends PowerLine {
             for (int i = 0; i < setSnKey.length; i++) {
                 String tmp = (String) deviceInfo.get(infoKey[i]);
                 res = sshClient.executeCmd(querySnKey[i]);
-                MainJFrame.showMssage("查询"+infoKey[i]+"设置结果 " + res);
+                MainJFrame.showMssage("查询" + infoKey[i] + "设置结果 " + res);
                 if (res != null && res.contains(tmp)) {
                     MainJFrame.showMssageln(infoKey[i] + "设置成功!");
                 } else {
@@ -90,36 +90,36 @@ public class WirelessRouteAndExpander extends PowerLine {
         generalSet = GeneralSet.getInstance();
         int port = connectParamSet.getUdpPort();
         String ip = connectParamSet.getIP();
-        String[] infoKeyUdp = {"mac_1","dak"};
-        String[] byname = {"PLC MAC","DAK"};
-        String[] messageType = {"0E","1E"};
+        String[] infoKeyUdp = {"mac_1", "dak"};
+        String[] byname = {"PLC MAC", "DAK"};
+        String[] messageType = {"0E", "1E"};
         ReqMessage reqMessage = new ReqMessage();
-        for(int i=0;i<messageType.length;i++){
+        for (int i = 0; i < messageType.length; i++) {
             String contenet = deviceInfo.get(infoKeyUdp[i]).toString();
             reqMessage.setType(messageType[i]);
             reqMessage.setContent(contenet);
             byte[] send = reqMessage.getMessage();
-            MainJFrame.showMssageln("设置"+byname[i]+"："+contenet+"\n" +
-                    "发送报文："+ ResMessage.parseByte2HexStr(send, 0, send.length));
+            MainJFrame.showMssageln("设置" + byname[i] + "：" + contenet + "\n" +
+                    "发送报文：" + ResMessage.parseByte2HexStr(send, 0, send.length));
             byte[] receive = sendMessage(udpClient, ip, port, send);
-            MainJFrame.showMssageln("收到报文："+ResMessage.parseByte2HexStr(receive,0,receive.length));
-            if("00010001".equals(ResMessage.parseByte2HexStr(receive, 0, 4))){
-                MainJFrame.showMssageln(byname[i]+"设置成功");
+            MainJFrame.showMssageln("收到报文：" + ResMessage.parseByte2HexStr(receive, 0, receive.length));
+            if ("00010001".equals(ResMessage.parseByte2HexStr(receive, 0, 4))) {
+                MainJFrame.showMssageln(byname[i] + "设置成功");
             }
         }
 
         MainJFrame.showMssageln("校验PLC MAC、DAK的设置结果...");
-        byte[][] queryMessage = {{0x02,0x00,0x00},{0x1F,0x00,0x00}};
-        for(int i=0;i<queryMessage.length;i++){
-            MainJFrame.showMssageln("发送"+byname[i]+"查询报文"+
-                    ResMessage.parseByte2HexStr(queryMessage[i],0,queryMessage[i].length));
+        byte[][] queryMessage = {{0x02, 0x00, 0x00}, {0x1F, 0x00, 0x00}};
+        for (int i = 0; i < queryMessage.length; i++) {
+            MainJFrame.showMssageln("发送" + byname[i] + "查询报文" +
+                    ResMessage.parseByte2HexStr(queryMessage[i], 0, queryMessage[i].length));
             byte[] receive = sendMessage(udpClient, ip, port, queryMessage[i]);
-            MainJFrame.showMssageln("收到报文："+ResMessage.parseByte2HexStr(receive,0,receive.length));
+            MainJFrame.showMssageln("收到报文：" + ResMessage.parseByte2HexStr(receive, 0, receive.length));
             ResMessage resMessage = new ResMessage(receive);
-            if(resMessage.queryRes.equals(deviceInfo.get(infoKey[i+2]))){
-                MainJFrame.showMssageln(byname[i]+"设置正确");
-            }else {
-                MainJFrame.showMssageln(resMessage.queryRes+"==="+deviceInfo.get(infoKey[i+2]));
+            if (resMessage.queryRes.equals(deviceInfo.get(infoKey[i + 2]))) {
+                MainJFrame.showMssageln(byname[i] + "设置正确");
+            } else {
+                MainJFrame.showMssageln(resMessage.queryRes + "===" + deviceInfo.get(infoKey[i + 2]));
                 return false;
             }
         }
@@ -128,21 +128,21 @@ public class WirelessRouteAndExpander extends PowerLine {
         MainJFrame.showMssageln("设置数据库,将wan、lan、wifi MAC地址写入数据库，并标注本条数据为已使用");
         Map<String, String> mac = new HashMap<>();
         String deviceMac = sshClient.executeCmd("link show");
-        deviceMac = deviceMac.replace("\n","");
+        deviceMac = deviceMac.replace("\n", "");
         String[] macArray = deviceMac.split(" ");
-        MainJFrame.showMssageln("#link show\n"+deviceMac);
-        if(null==deviceMac||"".equals(deviceMac)){
+        MainJFrame.showMssageln("#link show\n" + deviceMac);
+        if (null == deviceMac || "".equals(deviceMac)) {
             MainJFrame.showMssageln("无法获取设备MAC（wan、lan、wifi）地址！");
             return false;
         }
-        for(String str:macArray){
-            String tmp = str.substring(str.indexOf(":")+1);
-            String key = str.substring(0,str.indexOf(":"));
-            mac.put(key,tmp.replace(":", ""));
+        for (String str : macArray) {
+            String tmp = str.substring(str.indexOf(":") + 1);
+            String key = str.substring(0, str.indexOf(":"));
+            mac.put(key, tmp.replace(":", ""));
         }
-        if(serverInfo.setUsed("sn", (String) deviceInfo.get("sn"), mac)>0){
+        if (serverInfo.setUsed("sn", (String) deviceInfo.get("sn"), mac) > 0) {
             MainJFrame.showMssageln("数据库字段设置成功！");
-        }else {
+        } else {
             MainJFrame.showMssageln("数据库字段设置失败！");
             return false;
         }
@@ -227,21 +227,21 @@ public class WirelessRouteAndExpander extends PowerLine {
 //        ExtenderRes extenderRes = new ExtenderRes(bytes);
         String ssid = null;
 //        MainJFrame.showMssageln("广播报文9f 01 02 00 00 01 a3 1f，获取ssid：" + ssid);
-        if(moduleSelected==1){  ///电力线无线路由器
-            SSHClient sshClient = new SSHClient(connectParamSet.getUser(), connectParamSet.getPwd(),
-                    generalSet.getDevice_IP(), connectParamSet.getSshPort());
-            ssid = sshClient.executeCmd("uci get wireless.@wifi-iface[0].ssid").replace("\n","");
-            MainJFrame.showMssageln("执行uci get wireless.@wifi-iface[0].ssid命令，获取ssid：" + ssid);
-        }else {                 ///电力线无线扩展器
-            UDPClient udpClient = UDPClient.getInstance();
-            byte[] bytes = udpClient.sendPacket("255.255.255.255", generalSet.getUdpPort(), ReqMessage.hexStringToBytes("9f0102000001a31f"));
-            if(null==bytes){
-                return false;
-            }
-            ExtenderRes extenderRes = new ExtenderRes(bytes);
-            ssid = extenderRes.getSsid();
-            MainJFrame.showMssageln("广播报文9f 01 02 00 00 01 a3 1f，获取ssid：" + ssid);
-        }
+//        if(moduleSelected==1){  ///电力线无线路由器
+        SSHClient sshClient = new SSHClient(connectParamSet.getUser(), connectParamSet.getPwd(),
+                generalSet.getDevice_IP(), connectParamSet.getSshPort());
+        ssid = sshClient.executeCmd("uci get wireless.@wifi-iface[0].ssid").replace("\n", "");
+        MainJFrame.showMssageln("执行uci get wireless.@wifi-iface[0].ssid命令，获取ssid：" + ssid);
+//        }else {                 ///电力线无线扩展器
+//            UDPClient udpClient = UDPClient.getInstance();
+//            byte[] bytes = udpClient.sendPacket("255.255.255.255", generalSet.getUdpPort(), ReqMessage.hexStringToBytes("9f0102000001a31f"));
+//            if(null==bytes){
+//                return false;
+//            }
+//            ExtenderRes extenderRes = new ExtenderRes(bytes);
+//            ssid = extenderRes.getSsid();
+//            MainJFrame.showMssageln("广播报文9f 01 02 00 00 01 a3 1f，获取ssid：" + ssid);
+//        }
         //检测wifi，并获取信号强度
         String wifi_connectIP = null;
         String wifiStrength = Ping.getWifiStrength(ssid);
@@ -250,7 +250,7 @@ public class WirelessRouteAndExpander extends PowerLine {
         } else {
             MainJFrame.showMssageln("检测不到有关wifi " + ssid + "的信息...");
             int i = JOptionPane.showConfirmDialog(deviceTest,
-                    "检测不到有关wifi " + ssid+",是否继续", "提示",
+                    "检测不到有关wifi " + ssid + ",是否继续", "提示",
                     JOptionPane.YES_NO_CANCEL_OPTION);
             if (0 != i) {
                 return false;
@@ -284,7 +284,7 @@ public class WirelessRouteAndExpander extends PowerLine {
                     String alarmInfo = "请自行连接无线" + ssid + ",然后继续...";
                     int select = JOptionPane.showConfirmDialog(deviceTest, alarmInfo, "提示", JOptionPane.YES_NO_CANCEL_OPTION);
                     if (0 != select) {
-                        MainJFrame.showMssageln("无法连接wifi：" + ssid+",终止wifi测试");
+                        MainJFrame.showMssageln("无法连接wifi：" + ssid + ",终止wifi测试");
                         Ping.executeCmd(deleteConfig);
                         return false;
                     } else {
